2f8135f1103f85f64925b4c3e63321c006d25085
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-03 16:38:58 -0300

    adicão do componente gerenciador de rotas privadas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import HomeScreen from './screens/home/home';
-import RegisterScreen from './screens/auth/register/register'
-import LoginScreen from './screens/auth/login/login'
-import NotesIndexScreen from './screens/notes/notes'
-import UserEditScreen from './screens/users/edit/edit'
+import RegisterScreen from './screens/auth/register/register';
+import LoginScreen from './screens/auth/login/login';
+import NotesIndexScreen from './screens/notes/notes';
+import UserEditScreen from './screens/users/edit/edit';
+import PrivateRoute from "./components/auth/privateRoute/privateRoute";
 
 const Router = () => (
     <BrowserRouter>
@@ -11,8 +12,10 @@ const Router = () => (
             <Route exact path="/" element={ <HomeScreen/> } />
             <Route exact path="/register" element={ <RegisterScreen/> } />
             <Route exact path="/login" element={ <LoginScreen/> } />
-            <Route exact path="/notes" element={ <NotesIndexScreen/> } />
-            <Route exact path="/user/edit" element={ <UserEditScreen/> } />
+            <Route element={<PrivateRoute />}>
+                <Route exact path="/notes" element={ <NotesIndexScreen/> } />
+                <Route exact path="/user/edit" element={ <UserEditScreen/> } />
+            </Route>
         </Routes>
     </BrowserRouter>
 )
