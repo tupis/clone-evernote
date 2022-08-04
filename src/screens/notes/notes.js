8dd151d2f9e58b336d37a5727117001e0bb5febb
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-04 16:38:56 -0300

    criação de estado que define se sidebar está aberta + passando estados pros componentes filhos

--- src/screens/notes/notes.js
+++ src/screens/notes/notes.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import HeaderLogged from "../../components/header_logged/header_logged";
+import Notes from "../../components/notes/notes";
+
 
 const NotesScreen = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
         <>
-            <HeaderLogged />
+            <HeaderLogged isOpen={isOpen} setIsOpen={setIsOpen}/>
+            <Notes isOpen={isOpen} setIsOpen={setIsOpen}/>
         </>
     );
 }
