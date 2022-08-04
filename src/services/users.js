acfa35c682f2339791dde8c2a1fc507d8311193f
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-04 09:01:17 -0300

    adição do método de logout

--- src/services/users.js
+++ src/services/users.js
@@ -6,6 +6,10 @@ const UserServices = {
         const response = await Api.post('/users/login', params);
         localStorage.setItem('user', JSON.stringify(response.data.user));
         localStorage.setItem('token', JSON.stringify(response.data.token))
+    },
+    logout: () => {
+        localStorage.removeItem('user', null);
+        localStorage.removeItem('token', null)
     }
 };
 
