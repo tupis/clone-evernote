9c4564404c8a14ecd17c445bdc69a33dac785b55
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-04 16:37:46 -0300

    adição de menu hamburguer pro celular, e menu pra abrir a sidebar com função que define se está aberta ou não

--- src/components/header_logged/header_logged.js
+++ src/components/header_logged/header_logged.js
@@ -7,7 +7,7 @@ import { Navigate, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faList } from '@fortawesome/free-solid-svg-icons';
 
-const HeaderLogged = (props) => {
+const HeaderLogged = ({setIsOpen, isOpen}) => {
     const [redirectToHome, setRedirectToHome] = useState(false);
 
     const logOut = async () => {
@@ -21,42 +21,64 @@ const HeaderLogged = (props) => {
 
     return (
         <Navbar color="custom-purple" className="navbar-logged">
-        <Navbar.Brand>
-            <Column.Group>
-                <Column size="11" offset="1">
-                    <Link to="/notes">
-                        <img src={logoImage} onClick={() => setRedirectToHome(true)}/>
-                    </Link>
-                </Column>
-            </Column.Group>
-        </Navbar.Brand>
+            <Navbar.Brand>
+                <Column.Group>
+                    <Column size="11" offset="1">
+                        <Link to="/notes">
+                            <img src={logoImage} onClick={() => setRedirectToHome(true)}/>
+                        </Link>
+                    </Column>
+                </Column.Group>
+                <Navbar.Burger
+                    className="navbar-burger burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbar-menu"
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </Navbar.Burger>
+            </Navbar.Brand>
 
-        <Navbar.Menu>
-            <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
-                <Navbar.Item as="div">
-                    <Dropdown>
-                        <Dropdown.Trigger>
-                            <Button className="button" color="white" outlined>
-                                <span>Leonardo ▼</span>
-                            </Button>
-                        </Dropdown.Trigger>
-                            <Dropdown.Menu>
-                                <Dropdown.Content>
-                                    <Dropdown.Item as="div">
-                                        <Link to="/users/edit">User Edit</Link>
-                                    </Dropdown.Item>
+            <Navbar.Menu>
+                <Navbar.Segment as="div" className="navbar-item navbar-start" align="start">
+                    <Navbar.Item as="div">
+                        <Button
+                            className="open-button"
+                            color="white"
+                            outlined
+                            onClick={() => setIsOpen(!isOpen)}
+                        >
+                            <FontAwesomeIcon icon={faList} />
+                        </Button>
+                    </Navbar.Item>
+                </Navbar.Segment>
+                <Navbar.Segment as="div" className="navbar-item navbar-end" align="right">
+                    <Navbar.Item as="div">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <Button className="button" color="white" outlined>
+                                    <span>Tupi ▼</span>
+                                </Button>
+                            </Dropdown.Trigger>
+                                <Dropdown.Menu>
+                                    <Dropdown.Content>
+                                        <Dropdown.Item as="div">
+                                            <Link to="/users/edit">User Edit</Link>
+                                        </Dropdown.Item>
 
-                                    <Dropdown.Divider />
+                                        <Dropdown.Divider />
 
-                                    <Dropdown.Item as="div">
-                                        <a href="#" onClick={logOut}>LogOut</a>
-                                    </Dropdown.Item>
-                                </Dropdown.Content>
-                            </Dropdown.Menu>
-                    </Dropdown>
-                </Navbar.Item>
-            </Navbar.Segment>
-        </Navbar.Menu>
+                                        <Dropdown.Item as="div">
+                                            <a href="#" onClick={logOut}>LogOut</a>
+                                        </Dropdown.Item>
+                                    </Dropdown.Content>
+                                </Dropdown.Menu>
+                        </Dropdown>
+                    </Navbar.Item>
+                </Navbar.Segment>
+            </Navbar.Menu>
         </Navbar>
     )
 }
