2f7d2df43d440df51066dc3f056307bdf3e30e4e
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-09 01:04:01 -0300

    interação com o crud da API + método search para pesquisa de títulos

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,11 +1,14 @@
 import Api from './api'
 
+const headers = {'x-access-token': localStorage.getItem('token')}
+
 const NotesServices = {
 
-    index: () => Api.get('/notes', {
-        headers: {'x-access-token': localStorage.getItem('token')}
-    })
-
+    index: () => Api.get('/notes', { headers }),
+    create: (title, body) => Api.post('/notes', {'title': title, 'body': body}, { headers }),
+    delete: (id) => Api.delete(`/notes/${id}`, { headers }),
+    update: (id, params) => Api.put(`/notes/${id}`, params, { headers }),
+    search: (query) => Api.get(`/notes/search?query=${query}`, { headers })
 }
  
 export default NotesServices;
