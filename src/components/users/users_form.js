5128c82d930cd752b8c3b9322b53a4203021a926
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-09 01:13:01 -0300

    criação de componente que modifica e-mail e nome

---
+++ src/components/users/users_form.js
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react';
+import { Button, Field, Control, Input, Column, Help, Label } from "rbx";
+import UserServices from '../../services/users';
+
+const UsersEditForm = ({setNameHeader}) => {
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [status, setStatus] = useState(null);
+
+    const initializeUser = async () => {
+        const user = await JSON.parse(localStorage.getItem('user'));
+        setEmail(user.email);
+        setName(user.name);
+        setNameHeader(user.name)
+    }
+
+    useEffect(() =>{
+        initializeUser()
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await UserServices.update({ email, name });
+            setNameHeader(name)
+            setStatus("success")
+        } catch (err) {
+            setStatus("error")
+        }
+        setTimeout(()=>{
+            setStatus(null)
+        }, 3000)
+    }
+
+    return ( 
+        <>
+            <form onSubmit={handleSubmit}>
+                <Field>
+                    <Control>
+                        <Label className='has-text-grey'>Full Name</Label>
+                        <Input 
+                            type="name"
+                            value={name || ""}
+                            onChange={e => setName(e.target.value)}
+                            required
+                            name='name'
+                        />
+                    </Control>
+                </Field>
+
+
+                <Field>
+                    <Control>
+                        <Label className='has-text-grey'>Email</Label>
+                        <Input
+                            type='email'
+                            name='email'
+                            value={email || ''}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                        />
+                    </Control>
+                </Field>
+
+
+                <Field>
+                    <Control>
+                        <Column.Group>
+                            <Column className='has-text-rigth'>
+                                <Button color='custom-purple' outlined>Update</Button>
+                            </Column>
+                        </Column.Group>
+                    </Control>
+                </Field>
+                {status === 'success' &&
+                    <Help color='primary'>Updated with success</Help>
+                }
+                {status === 'error' &&
+                    <Help color='danger'>Failed to update</Help>
+                }
+            </form>
+        </>
+    )
+}
+
+
+export default UsersEditForm;
