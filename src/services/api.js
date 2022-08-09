b4a9a507a4864d318d55f1fb887da9ff9014c022
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-09 19:33:36 -0300

    ajuste de ambiente variavel para deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let config = require('../env-config.json');
 
-const Api = axios.create({baseURL: config.baseURL} || process.env.BASE_URL)
+const Api = axios.create(process.env.BASE_URL)
 
 export default Api;
