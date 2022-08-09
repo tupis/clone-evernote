27e05e76d31d715e9b0b71967f4896772b5e0e64
João Tupinambá <95971013+tupis@users.noreply.github.com>
2022-08-09 18:15:28 -0300

    adição de variavel ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 let config = require('../env-config.json');
 
-const Api = axios.create({baseURL: config.baseURL})
+const Api = axios.create({baseURL: config.baseURL} || process.env.BASE_URL)
 
 export default Api;
